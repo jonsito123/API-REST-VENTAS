240d4aa75d354f092b1ab80cf0b9487e3270742b
jonsito123 <ecuevah70@gmail.com>
2026-01-13 10:11:02 -0500

    AddFieldCpm

--- src/controllers/MedicosController.js
+++ src/controllers/MedicosController.js
@@ -14,21 +14,20 @@ export const ObtenerInfomacionMedico=async(req,res)=>{
 
 export const ObtenerMedicosEspecialidad=async(req,res)=>{
 
-
     const especialidad=req.params.id;
 
 
-const [rows] =await  pool.query("SELECT  M.id_medico ,M.CPM,Nombres,Apellidos,Descripcion as Especialidad FROM Medico AS M inner join Especialidad as E  ON E.id_especialidad=M.id_especialidad where E.id_especialidad=?",[especialidad]);
+    const [rows] =await  pool.query("SELECT M.id_medico,M.CMP,Nombres,Apellidos,Descripcion as Especialidad FROM Medico AS M inner join Especialidad as E  ON E.id_especialidad=M.id_especialidad where E.id_especialidad=?",[especialidad]);
  
-if(rows.length<=0 || rows.length===0){
-     return res.status(404).json({ 
-            mensaje: "No existe medico con ese ID" 
-        });
+    if(rows.length<=0 || rows.length===0){
+        return res.status(404).json({ 
+                mensaje: "No existe medico con ese ID" 
+            });
 
-}
+    }
 
- res.json(rows) 
-}
+    res.json(rows) 
+    }
 
 
 
