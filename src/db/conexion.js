269bf23cae1c3746aba3e8cb1fa318a14676c0ab
Edwin Cueva Huaman <86698943+jonsito123@users.noreply.github.com>
2026-04-27 10:34:43 -0500

    Update conexion.js

--- src/db/conexion.js
+++ src/db/conexion.js
@@ -1,7 +1,7 @@
 import {createPool} from "mysql2/promise"
 
 export const pool=createPool({
-host: "162.222.225.165",
+host: "170.249.205.58",
 user: "clinijdb_gallardo",
 password: "Fresnos2025%",
 database: "clinijdb_dbfresalud"
