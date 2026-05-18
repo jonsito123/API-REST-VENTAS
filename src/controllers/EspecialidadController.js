a1f9338c1ae79fed0080ee11db82062c46acdc02
Edwin Cueva Huaman <86698943+jonsito123@users.noreply.github.com>
2026-05-18 08:44:02 -0500

    FechaHoraFinal-Especialidad

--- src/controllers/EspecialidadController.js
+++ src/controllers/EspecialidadController.js
@@ -4,7 +4,7 @@ export const GetEspecialidades=async(req,res)=>{
     try {
      
 
-        const [result] =await  pool.query("Select Especialidad.id_especialidad,Especialidad.Descripcion from Especialidad WHERE   Especialidad.id_especialidad in (select id_especialidad from Medico as M inner join HorariosMedico AS HM on M.id_medico=HM.id_medico where HM.FechaHorario>=CURDATE())");
+        const [result] =await  pool.query("Select Especialidad.id_especialidad,Especialidad.Descripcion from Especialidad WHERE   Especialidad.id_especialidad in (select id_especialidad from Medico as M inner join HorariosMedico AS HM on M.id_medico=HM.id_medico where HM.FechaHorario>=CURRENT_DATE())");
         res.send(result)
 
     }catch(error){
