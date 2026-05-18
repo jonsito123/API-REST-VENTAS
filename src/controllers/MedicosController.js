834520b001e9efbfd27506854cbeec31fa9eb0be
Edwin Cueva Huaman <86698943+jonsito123@users.noreply.github.com>
2026-05-18 09:05:48 -0500

    MedicosEspelidadConsultaFiltro

--- src/controllers/MedicosController.js
+++ src/controllers/MedicosController.js
@@ -17,7 +17,7 @@ export const ObtenerMedicosEspecialidad=async(req,res)=>{
     const especialidad=req.params.id;
 
 
-    const [rows] =await  pool.query("SELECT M.id_medico,M.CMP,Nombres,Apellidos,Descripcion as Especialidad FROM Medico AS M inner join Especialidad as E  ON E.id_especialidad=M.id_especialidad where E.id_especialidad=?",[especialidad]);
+    const [rows] =await  pool.query("SELECT M.id_medico,M.CMP,Nombres,Apellidos,Descripcion as Especialidad FROM Medico AS M inner join Especialidad as E  ON E.id_especialidad=M.id_especialidad where  M.id_medico in (select M.id_medico from Medico as M inner join HorariosMedico AS HM on M.id_medico=HM.id_medico where HM.FechaHorario >=CURRENT_DATE() AND id_especialidad=?)",[especialidad]);
  
     if(rows.length<=0 || rows.length===0){
         return res.status(404).json({ 
