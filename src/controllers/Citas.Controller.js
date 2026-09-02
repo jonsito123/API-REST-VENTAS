6fb94094bbeac1b2db371771952ab786d1205fea
Edwin Cueva Huaman <86698943+jonsito123@users.noreply.github.com>
2026-09-02 09:44:24 -0500

    Asunto Reserva Agendada

--- src/controllers/Citas.Controller.js
+++ src/controllers/Citas.Controller.js
@@ -82,7 +82,7 @@ try {
 
         const sendSmtpEmail = new brevo.SendSmtpEmail();
 
-        sendSmtpEmail.subject = "Registro de Reserva Exitoso";
+        sendSmtpEmail.subject = "Reserva Agendada";
         sendSmtpEmail.to = [
         { email: Correo, name:PacienteNombres},
         //   { email: "fazttech@gmail.com", name: "Joe Mcmillan" },
